31f297ccec810a4b96939309978429ccaf0c4699
Tomek <rybawoda2@gmail.com>
2022-05-24 22:07:34 +0500

    Initial commit

---
+++ Assets/zapalkaController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zapalkaController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        Debug.Log("Dziala");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
