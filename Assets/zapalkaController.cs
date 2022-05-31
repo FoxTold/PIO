0c2f44e3b82eb3356f233a0201c72b9ee9ab24ee
Ryba <t.b.rybinski@gmail.com>
2022-05-31 19:15:32 +0500

    GameLogic added

--- Assets/zapalkaController.cs
+++ Assets/zapalkaController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class zapalkaController : MonoBehaviour
 {
+    public GameController gamecontroller;
     // Start is called before the first frame update
     private void OnMouseDown()
     {
-        Debug.Log("Dziala");
-        Destroy(this.gameObject);
+        if (gamecontroller.getNumberOfMoves() > 0)
+        {
+            Debug.Log(gamecontroller.getNumberOfMoves()-1);
+            gamecontroller.removeZapalka(this.gameObject);
+            gamecontroller.decrementNumberOfMoves();
+            Destroy(gameObject);
+        }
+
     }
-    void Start()
+    private void Start()
     {
-        
+        gamecontroller=FindObjectOfType<GameController>().GetComponent<GameController>();
     }
 
     // Update is called once per frame
