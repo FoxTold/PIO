2f086e39dbe2cc42597f0eb2147ebe6acd3703c4
rIBA <235960@edu.p.lodz.pl>
2022-06-01 13:21:24 +0200

    sprint3

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,24 +5,32 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject prefab;
+    public  GameObject prefab;
     private GameObject spawnPoint;
     public int max_Zapalek = 10;
     private List<GameObject> listOfZapalki = new List<GameObject>();
 
     private int current_Zapalek=0;
 
-    private string gracz1 = "gracz1";
-    private string gracz2 = "gracz2";
+    private const string gracz1 = "Player1";
+    private const string gracz2 = "Player2";
 
     private string current_Gracz;
 
     private string winner;
 
     private int possibleMoves = 3;
+    public GameObject napis;
+    private Text txt;
+
+    public GameObject winNapis;
+    private Text winText;
+
     // Start is called before the first frame update
     void Start()
     {
+        winText = winNapis.GetComponent<Text>();
+        txt = napis.GetComponent<Text>();
         current_Gracz = gracz1;
         current_Zapalek = max_Zapalek;
         spawnPoint = this.gameObject;
@@ -32,6 +40,7 @@ public class GameController : MonoBehaviour
             zap.transform.position = spawnPoint.transform.position;
             listOfZapalki.Add(zap); 
         }
+        txt.text = "Current player: " + current_Gracz;
 
     }
     public void removeZapalka(GameObject zapalka)
@@ -46,6 +55,7 @@ public class GameController : MonoBehaviour
         {
             winner = current_Gracz ;
             Debug.Log("Wygral: " + winner);
+            winText.text = "Winner: " + winner;
             current_Zapalek = 100000;
         }
     }
@@ -55,6 +65,7 @@ public class GameController : MonoBehaviour
         possibleMoves = 3;
         current_Gracz = current_Gracz == gracz1 ? gracz2 : gracz1;
         Debug.Log("Current player: " + current_Gracz);
+        txt.text = "Current player: " + current_Gracz;
         Debug.Log(current_Zapalek);
     }
 
