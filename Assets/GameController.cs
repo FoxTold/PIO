6aaca7b017b3033dffb825b7e36ed8c8efbb6242
JakubBernaciak <235830@edu.p.lodz.pl>
2022-06-07 16:03:50 +0200

    New Texture

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -30,14 +30,24 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject PlayAgain;
     [SerializeField] private GameObject Menu;
 
+    [SerializeField] private GameObject match1;
+    [SerializeField] private GameObject match2;
+    [SerializeField] private GameObject match3;
+
+    [SerializeField] private GameObject counter;
+    private Text counter_txt;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        counter_txt = counter.GetComponent<Text>();
         button.SetActive(false);
         PlayAgain.SetActive(false);
         Menu.SetActive(false);
         winText = winNapis.GetComponent<Text>();
         txt = napis.GetComponent<Text>();
+
         current_Gracz = gracz1;
         current_Zapalek = max_Zapalek;
         spawnPoint = this.gameObject;
@@ -47,11 +57,19 @@ public class GameController : MonoBehaviour
             zap.transform.position = spawnPoint.transform.position;
             listOfZapalki.Add(zap); 
         }
-        txt.text = "Current player: " + current_Gracz;
+        txt.text =  current_Gracz;
 
     }
     public void removeZapalka(GameObject zapalka)
     {
+        updateCounter();
+        if (possibleMoves <= 3)
+            match3.SetActive(false);
+        if (possibleMoves <= 2)
+            match2.SetActive(false);
+        if (possibleMoves <= 1)
+            match1.SetActive(false);
+
         listOfZapalki.Remove(zapalka);
         button.SetActive(true);
         current_Zapalek--;
@@ -59,8 +77,10 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        updateCounter();
         if (current_Zapalek <=0)
         {
+            counter.SetActive(false);
             PlayAgain.SetActive(true);
             Menu.SetActive(true);
             winner = current_Gracz ;
@@ -68,6 +88,8 @@ public class GameController : MonoBehaviour
             txt.text = "";
             winText.text = "Winner: " + winner;
             current_Zapalek = 100000;
+            match1.SetActive(false);
+            match2.SetActive(false);
             Destroy(button);
         }
     }
@@ -79,9 +101,12 @@ public class GameController : MonoBehaviour
             possibleMoves = 3;
             current_Gracz = current_Gracz == gracz1 ? gracz2 : gracz1;
             Debug.Log("Current player: " + current_Gracz);
-            txt.text = "Current player: " + current_Gracz;
+            txt.text = current_Gracz;
             Debug.Log(current_Zapalek);
             button.SetActive(false);
+            match1.SetActive(true);
+            match2.SetActive(true);
+            match3.SetActive(true);
         }
     }
 
@@ -92,5 +117,9 @@ public class GameController : MonoBehaviour
     public void decrementNumberOfMoves()
     {
         possibleMoves--;
+    }
+    public void updateCounter()
+    {
+        counter_txt.text = "Matches left: "+current_Zapalek;
     }
 }
